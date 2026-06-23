625cbda3e45c352b4f5db4c4610e892686b6c7c0
Venkat Sai Adarsh Challagalla <challagallavenkatsaiadarsh@gmail.com>
2026-06-23 13:24:02 +0530

    fix kivy CI boot + disable window in github actions

--- frontend/boot.py
+++ frontend/boot.py
@@ -1,5 +1,9 @@
 import os
 
 def safe_kivy_boot():
-    os.environ["KIVY_WINDOW"] = "sdl2"
-    os.environ["KIVY_GL_BACKEND"] = "sdl2"
+    # ================= CI / GITHUB ACTIONS SAFE MODE =================
+    if os.environ.get("CI") == "true":
+        os.environ["KIVY_NO_ARGS"] = "1"
+        os.environ["KIVY_WINDOW"] = "dummy"
+        os.environ["KIVY_GL_BACKEND"] = "mock"
+        os.environ["SDL_VIDEODRIVER"] = "dummy"
